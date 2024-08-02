d2291305cdb4033bf20e8219d9db0f3595bcddd0
YoungpilYun <youngfeeli@gmail.com>
2024-08-03 08:24:41 +0900

    코인 선택 드롭다운버튼 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import Plot from "react-plotly.js";
 import axios from "axios";
+import { Dropdown, DropdownButton, FormCheck } from "react-bootstrap";
+import { SMA, BollingerBands } from "technicalindicators";
 
 const intervals = {
   "1일": "days",
@@ -17,8 +19,10 @@ const intervals = {
   "4시간": "minutes/240",
 };
 
-const fetchData = async (type = "days", count = 200, to = null, from = null) => {
-  const url = `https://api.upbit.com/v1/candles/${type}?market=KRW-BTC&count=${count}${to ? `&to=${to}` : ""}${
+const indicators = ["볼륨", "이동평균선", "볼린저 밴드"];
+
+const fetchData = async (type = "days", market = "KRW-BTC", count = 200, to = null, from = null) => {
+  const url = `https://api.upbit.com/v1/candles/${type}?market=${market}&count=${count}${to ? `&to=${to}` : ""}${
     from ? `&from=${from}` : ""
   }`;
 
@@ -41,23 +45,70 @@ const fetchData = async (type = "days", count = 200, to = null, from = null) => 
   }
 };
 
+const calculateMovingAverage = (data, period) => {
+  return SMA.calculate({ period: period, values: data });
+};
+
+const calculateBollingerBands = (data, period, stdDev) => {
+  return BollingerBands.calculate({
+    period: period,
+    values: data,
+    stdDev: stdDev,
+  });
+};
+
 const App = () => {
+  const [marketList, setMarketList] = useState([]);
+  const [selectedMarket, setSelectedMarket] = useState("KRW-BTC");
   const [chartData, setChartData] = useState([]);
   const [displayData, setDisplayData] = useState([]);
+  const [ma20Data, setMa20Data] = useState([]);
+  const [ma60Data, setMa60Data] = useState([]);
+  const [bbData, setBbData] = useState([]);
   const [selectedInterval, setSelectedInterval] = useState("days");
   const [hasMorePastData, setHasMorePastData] = useState(true);
   const [hasMoreFutureData, setHasMoreFutureData] = useState(true);
+  const [selectedIndicators, setSelectedIndicators] = useState(["볼륨"]);
+
+  useEffect(() => {
+    const fetchMarkets = async () => {
+      const url = "https://api.upbit.com/v1/market/all";
+      try {
+        const response = await axios.get(url);
+        const krwMarkets = response.data.filter((market) => market.market.startsWith("KRW-"));
+        setMarketList(krwMarkets);
+      } catch (error) {
+        console.error("Error fetching market data: ", error);
+      }
+    };
+
+    fetchMarkets();
+  }, []);
 
   useEffect(() => {
     const getData = async () => {
-      const data = await fetchData(selectedInterval);
+      const data = await fetchData(selectedInterval, selectedMarket);
       setChartData(data);
+
+      const closePrices = data.map((d) => d.close);
+      const ma20 = calculateMovingAverage(closePrices, 20);
+      const ma60 = calculateMovingAverage(closePrices, 60);
+      const bb = calculateBollingerBands(closePrices, 20, 2); // 20-period Bollinger Bands with 2 standard deviations
+
+      const ma20FullData = new Array(data.length - ma20.length).fill(null).concat(ma20);
+      const ma60FullData = new Array(data.length - ma60.length).fill(null).concat(ma60);
+      const bbFullData = new Array(data.length - bb.length).fill(null).concat(bb);
+
+      setMa20Data(ma20FullData);
+      setMa60Data(ma60FullData);
+      setBbData(bbFullData);
+
       setDisplayData(data.slice(-100)); // 초기에는 최신 100개 데이터만 표시
       console.log(data);
     };
 
     getData();
-  }, [selectedInterval]);
+  }, [selectedInterval, selectedMarket]);
 
   const fetchMoreData = async (direction) => {
     if (!chartData.length) return;
@@ -65,7 +116,7 @@ const App = () => {
     if (direction === "past" && hasMorePastData) {
       const oldestDate = chartData[0].date;
       const toTimestamp = oldestDate.toISOString();
-      const moreData = await fetchData(selectedInterval, 100, toTimestamp);
+      const moreData = await fetchData(selectedInterval, selectedMarket, 100, toTimestamp);
 
       if (moreData.length) {
         setChartData((prevData) => [...moreData, ...prevData]);
@@ -75,7 +126,7 @@ const App = () => {
     } else if (direction === "future" && hasMoreFutureData) {
       const newestDate = chartData[chartData.length - 1].date;
       const fromTimestamp = newestDate.toISOString();
-      const moreData = await fetchData(selectedInterval, 100, null, fromTimestamp);
+      const moreData = await fetchData(selectedInterval, selectedMarket, 100, null, fromTimestamp);
 
       if (moreData.length) {
         setChartData((prevData) => [...prevData, ...moreData]);
@@ -115,79 +166,179 @@ const App = () => {
     }
   };
 
-  const handleIntervalChange = (event) => {
-    setSelectedInterval(intervals[event.target.value]);
+  const handleIntervalChange = (intervalKey) => {
+    setSelectedInterval(intervals[intervalKey]);
   };
 
-  const trace1 = {
-    x: displayData.map((d) => d.date),
-    close: displayData.map((d) => d.close),
-    decreasing: { line: { color: "blue" } },
-    high: displayData.map((d) => d.high),
-    increasing: { line: { color: "red" } },
-    low: displayData.map((d) => d.low),
-    open: displayData.map((d) => d.open),
-    type: "candlestick",
-    xaxis: "x",
-    yaxis: "y",
+  const handleMarketChange = (market) => {
+    setSelectedMarket(market);
   };
 
-  const trace2 = {
-    x: displayData.map((d) => d.date),
-    y: displayData.map((d) => d.volume),
-    type: "bar",
-    xaxis: "x",
-    yaxis: "y2",
-    marker: {
-      color: "rgba(31,119,180,0.5)",
-    },
+  const handleIndicatorChange = (indicator) => {
+    setSelectedIndicators((prevSelected) =>
+      prevSelected.includes(indicator) ? prevSelected.filter((i) => i !== indicator) : [...prevSelected, indicator]
+    );
   };
 
-  const layout = {
-    dragmode: "pan",
-    margin: {
-      r: 10,
-      t: 25,
-      b: 40,
-      l: 60,
-    },
-    showlegend: false,
-    xaxis: {
-      autorange: true,
-      domain: [0, 1],
-      title: "Date",
-      type: "date",
-      rangeslider: { visible: false },
-    },
-    yaxis: {
-      autorange: true,
-      domain: [0.3, 1],
-      type: "linear",
-    },
-    yaxis2: {
-      autorange: true,
-      domain: [0, 0.2],
-      type: "linear",
-    },
-    grid: {
-      rows: 2,
-      columns: 1,
-      subplots: [["xy"], ["xy2"]],
-    },
+  const getSelectedIntervalLabel = () => {
+    return Object.keys(intervals).find((key) => intervals[key] === selectedInterval);
   };
 
   return (
     <div className="App">
-      <select onChange={handleIntervalChange}>
-        {Object.keys(intervals).map((key) => (
-          <option key={key} value={key}>
-            {key}
-          </option>
-        ))}
-      </select>
+      <div className="dropdown-container">
+        <DropdownButton id="dropdown-indicator-button" title="지표" className="dropdown-button">
+          {indicators.map((indicator) => (
+            <Dropdown.Item key={indicator} onClick={() => handleIndicatorChange(indicator)}>
+              <FormCheck
+                type="checkbox"
+                label={indicator}
+                checked={selectedIndicators.includes(indicator)}
+                onChange={() => handleIndicatorChange(indicator)}
+              />
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
+        <DropdownButton id="dropdown-interval-button" title={getSelectedIntervalLabel()} className="dropdown-button">
+          {Object.keys(intervals).map((key) => (
+            <Dropdown.Item key={key} onClick={() => handleIntervalChange(key)}>
+              {key}
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
+        <DropdownButton id="dropdown-market-button" title={selectedMarket} className="dropdown-button">
+          {marketList.map((market) => (
+            <Dropdown.Item key={market.market} onClick={() => handleMarketChange(market.market)}>
+              {market.market}
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
+      </div>
       <Plot
-        data={[trace1, trace2]}
-        layout={layout}
+        data={[
+          {
+            x: displayData.map((d) => d.date),
+            close: displayData.map((d) => d.close),
+            decreasing: { line: { color: "blue" } },
+            high: displayData.map((d) => d.high),
+            increasing: { line: { color: "red" } },
+            low: displayData.map((d) => d.low),
+            open: displayData.map((d) => d.open),
+            type: "candlestick",
+            xaxis: "x",
+            yaxis: "y",
+          },
+          ...(selectedIndicators.includes("볼륨")
+            ? [
+                {
+                  x: displayData.map((d) => d.date),
+                  y: displayData.map((d) => d.volume),
+                  type: "bar",
+                  xaxis: "x",
+                  yaxis: "y2",
+                  marker: {
+                    color: "rgba(31,119,180,0.5)",
+                  },
+                },
+              ]
+            : []),
+          ...(selectedIndicators.includes("이동평균선")
+            ? [
+                {
+                  x: displayData.map((d) => d.date),
+                  y: displayData.map((d) => {
+                    const index = chartData.findIndex((cd) => cd.date.getTime() === d.date.getTime());
+                    return ma20Data[index];
+                  }),
+                  type: "scatter",
+                  mode: "lines",
+                  line: { color: "red" },
+                  name: "MA 20",
+                },
+                {
+                  x: displayData.map((d) => d.date),
+                  y: displayData.map((d) => {
+                    const index = chartData.findIndex((cd) => cd.date.getTime() === d.date.getTime());
+                    return ma60Data[index];
+                  }),
+                  type: "scatter",
+                  mode: "lines",
+                  line: { color: "blue" },
+                  name: "MA 60",
+                },
+              ]
+            : []),
+          ...(selectedIndicators.includes("볼린저 밴드")
+            ? [
+                {
+                  x: displayData.map((d) => d.date),
+                  y: displayData.map((d) => {
+                    const index = chartData.findIndex((cd) => cd.date.getTime() === d.date.getTime());
+                    return bbData[index]?.middle;
+                  }),
+                  type: "scatter",
+                  mode: "lines",
+                  line: { color: "red" },
+                  name: "BB Middle",
+                },
+                {
+                  x: displayData.map((d) => d.date),
+                  y: displayData.map((d) => {
+                    const index = chartData.findIndex((cd) => cd.date.getTime() === d.date.getTime());
+                    return bbData[index]?.upper;
+                  }),
+                  type: "scatter",
+                  mode: "lines",
+                  line: { color: "black" },
+                  name: "BB Upper",
+                },
+                {
+                  x: displayData.map((d) => d.date),
+                  y: displayData.map((d) => {
+                    const index = chartData.findIndex((cd) => cd.date.getTime() === d.date.getTime());
+                    return bbData[index]?.lower;
+                  }),
+                  type: "scatter",
+                  mode: "lines",
+                  line: { color: "black" },
+                  name: "BB Lower",
+                },
+              ]
+            : []),
+        ]}
+        layout={{
+          dragmode: "pan",
+          width: 990,
+          margin: {
+            r: 10,
+            t: 25,
+            b: 40,
+            l: 60,
+          },
+          showlegend: true,
+          xaxis: {
+            autorange: true,
+            domain: [0, 1],
+            title: "Date",
+            type: "date",
+            rangeslider: { visible: false },
+          },
+          yaxis: {
+            autorange: true,
+            domain: [0.3, 1],
+            type: "linear",
+          },
+          yaxis2: {
+            autorange: true,
+            domain: [0, 0.2],
+            type: "linear",
+          },
+          grid: {
+            rows: 2,
+            columns: 1,
+            subplots: [["xy"], ["xy2"]],
+          },
+        }}
         onRelayout={handleRelayout}
         config={{ modeBarButtonsToAdd: ["pan2d"], displayModeBar: true }}
       />
